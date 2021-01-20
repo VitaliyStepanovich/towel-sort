772308c4ac1bfcbb45ddc13be12eeddf800fd4f7
VitaliyStepanovich <vitaliys.garshin@gmail.com>
2021-01-21 02:04:09 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -3,5 +3,13 @@
 
 module.exports = function towelSort (matrix) {
 
-  return matrix;
+  if (typeof(matrix) == 'undefined' || matrix.length == 0) return [];
+  
+  let result = [];	
+  for (let i = 0; i < matrix.length; i++) {
+    result = result.concat(i % 2 == 0 ? matrix[i] : matrix[i].reverse());
+  }
+
+  return result;
+
 }
